f0eddead2eb69618aa2b3e52c38379e4ab831a62
xpertjiteachme <vrj992@gmail.com>
2022-02-27 10:00:05 +0530

    pandas few more code added

---
+++ Pandas-Tutorial/6.1How-To-Read-csv-file-in-pandas.py
@@ -0,0 +1,3 @@
+import pandas as pd
+df = pd.read_csv('C:/Users/xpertji/Desktop/folder/sample.csv')
+print(df)
---
+++ Pandas-Tutorial/6.2pandas-head()-function.py
@@ -0,0 +1,6 @@
+import pandas as pd
+df = pd.read_csv('C:/Users/xpertji/Desktop/folder/sample.csv')
+#print(df)
+
+#head() function in pandas read top rows
+df.head()
